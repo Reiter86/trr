71c4e73663f93129ae43b90593c20aa681cc3ffa
Roman Reiter <roman.reiter@studierende.htl-donaustadt.at>
2020-08-29 19:55:35 +0200

    add all files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Debug.WriteLine("#######Invalid object. ");
+        }
+    }
+}
